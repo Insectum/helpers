48671a7891b8bf2488e70af6b2a481a5852c4b3a
Terion <mail@terion.name>
2014-01-19 15:36:19 +0200

    draft

---
+++ src/Insectum/Helpers/Arr.php
@@ -0,0 +1,142 @@
+<?php
+
+
+namespace Insectum\Helpers;
+
+
+/**
+ * Class Arr
+ * @package Insectum\InsectumClient\Helpers
+ * @author Taylor Otwell <taylorotwell@gmail.com>
+ * @author Volodymyr Kornilov <mail@terion.name>
+ */
+class Arr
+{
+
+    /**
+     * Flatten a multi-dimensional associative array with dots.
+     *
+     * @param  array $array
+     * @param  string $prepend
+     * @return array
+     */
+    static function dot($array, $prepend = '')
+    {
+        $results = array();
+
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $results = array_merge($results, self::dot($value, $prepend . $key . '.'));
+            } else {
+                $results[$prepend . $key] = $value;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array $array
+     * @param  string $key
+     * @param  mixed $default
+     * @return mixed
+     */
+    static function get($array, $key, $default = null)
+    {
+        if (is_null($key)) return $array;
+
+        if (isset($array[$key])) return $array[$key];
+
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return value($default);
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
+    }
+
+    /**
+     * Remove an array item from a given array using "dot" notation.
+     *
+     * @param  array $array
+     * @param  string $key
+     * @return void
+     */
+    static function forget(&$array, $key)
+    {
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1) {
+            $key = array_shift($keys);
+
+            if (!isset($array[$key]) || !is_array($array[$key])) {
+                return;
+            }
+
+            $array =& $array[$key];
+        }
+
+        unset($array[array_shift($keys)]);
+    }
+
+    /**
+     * Get a subset of the items from the given array.
+     *
+     * @param  array $array
+     * @param  array $keys
+     * @return array
+     */
+    static function only($array, $keys)
+    {
+        return array_intersect_key($array, array_flip((array)$keys));
+    }
+
+    /**
+     * Pluck an array of values from an array.
+     *
+     * @param  array $array
+     * @param  string $value
+     * @param  string $key
+     * @return array
+     */
+    static function pluck($array, $value, $key = null)
+    {
+        $results = array();
+
+        foreach ($array as $item) {
+            $itemValue = is_object($item) ? $item->{$value} : $item[$value];
+
+            // If the key is "null", we will just append the value to the array and keep
+            // looping. Otherwise we will key the array using the value of the key we
+            // received from the developer. Then we'll return the final array form.
+            if (is_null($key)) {
+                $results[] = $itemValue;
+            } else {
+                $itemKey = is_object($item) ? $item->{$key} : $item[$key];
+
+                $results[$itemKey] = $itemValue;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Get all of the given array except for a specified array of items.
+     *
+     * @param  array  $array
+     * @param  array  $keys
+     * @return array
+     */
+    public static function except($array, $keys)
+    {
+        return array_diff_key($array, array_flip((array)$keys));
+    }
+
+
+}
